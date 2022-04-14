c4a397b8fdc713e297c8bade65e921ba208ec618
Pixxl <oliverhalenius@yahoo.se>
2022-04-14 02:12:21 +0200

    move most parts of PipeConnector into Connector

--- Space Refinery Game/Connector.cs
+++ Space Refinery Game/Connector.cs
@@ -1,13 +1,147 @@
-﻿using System;
+﻿using FixedPrecision;
+using FXRenderer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Veldrid;
 
 namespace Space_Refinery_Game
 {
-	public abstract class Connector : Entity
+	public abstract class Connector : Entity, IDisposable
 	{
+		public Connector((IConnectable connectableA, IConnectable connectableB) connectables) : this()
+		{
+			Connectables = connectables;
+		}
+
+		public Connector(IConnectable initialConnectable, ConnectorSide side) : this()
+		{
+			Connectables = (side == ConnectorSide.A ? (initialConnectable, null) : (null, initialConnectable));
+
+			VacantSide = (side == ConnectorSide.A ? ConnectorSide.B : ConnectorSide.A);
+		}
+
+		protected Connector()
+		{
+			MainGame.DebugRender.AddDebugObjects += AddDebugObjects;
+		}
+
+		public (IConnectable? connectableA, IConnectable? connectableB) Connectables { get; protected set; }
+
+		public IConnectable? Unconnected
+		{
+			get
+			{
+				if (!VacantSide.HasValue)
+				{
+					return null;
+				}
+
+				return (VacantSide == ConnectorSide.A ? Connectables.connectableB : Connectables.connectableA);
+			}
+		}
+
+		public void Connect(IConnectable connectable)
+		{
+			if (!Vacant)
+			{
+				throw new Exception($"{nameof(Connector)} is not vacant.");
+			}
+
+			if (VacantSide == ConnectorSide.A)
+			{
+				Connectables = (connectable, Connectables.connectableB);
+
+				if (Connectables.connectableB is not null)
+				{
+					VacantSide = null;
+				}
+			}
+			else if (VacantSide == ConnectorSide.B)
+			{
+				Connectables = (Connectables.connectableA, connectable);
+
+				if (Connectables.connectableA is not null)
+				{
+					VacantSide = null;
+				}
+			}
+		}
+
+		public void Disconnect(ConnectorSide side)
+		{
+			if (side == ConnectorSide.A)
+			{
+				Connectables = (null, Connectables.connectableB);
+			}
+			else if (side == ConnectorSide.B)
+			{
+				Connectables = (Connectables.connectableA, null);
+			}
+			VacantSide = side;
+
+			if (Connectables.connectableA is null && Connectables.connectableB is null)
+			{
+				Dispose();
+			}
+		}
+
+		public void Disconnect(IConnectable connectable)
+		{
+			if (Connectables.connectableA == connectable)
+			{
+				Disconnect(ConnectorSide.A);
+			}
+			else if (Connectables.connectableB == connectable)
+			{
+				Disconnect(ConnectorSide.B);
+			}
+		}
+
+		public Transform Transform;
+
+		public ConnectorProxy Proxy;
+
+		public PhysicsObject PhysicsObject;
+
+		public ConnectorSide? VacantSide;
+
 		public abstract IInformationProvider InformationProvider { get; }
+
+		public bool Vacant => VacantSide is not null;
+
+		public ConnectorSide? PopulatedSide
+		{
+			get
+			{
+				if (!VacantSide.HasValue)
+				{
+					return null;
+				}
+
+				return (VacantSide == ConnectorSide.A ? ConnectorSide.B : ConnectorSide.A);
+			}
+		}
+
+		private void AddDebugObjects()
+		{
+			if (!MainGame.DebugSettings.AccessSetting<BooleanSetting>($"{nameof(Connector)} debug objects"))
+				return;
+
+			MainGame.DebugRender.DrawOrientationMarks(PhysicsObject.Transform);
+
+			MainGame.DebugRender.DrawCube(new Transform(PhysicsObject.Transform) { Scale = new((FixedDecimalInt4).4f, (FixedDecimalInt4).4f, (FixedDecimalInt4).25f) }, VacantSide is null ? RgbaFloat.Green : RgbaFloat.Cyan);
+		}
+
+		public void Dispose()
+		{
+			PhysicsObject.Destroy();
+
+			Proxy.PhysicsObject.Destroy();
+
+			MainGame.DebugRender.AddDebugObjects -= AddDebugObjects;
+		}
 	}
 }
--- Space Refinery Game/PipeConnector.cs
+++ Space Refinery Game/PipeConnector.cs
@@ -6,133 +6,15 @@ namespace Space_Refinery_Game
 {
 	public class PipeConnector : Connector
 	{
-		public PipeConnector((Pipe pipeA, Pipe pipeB) pipes) : this()
+		public PipeConnector((Pipe connectableA, Pipe connectableB) connectables) : base(connectables)
 		{
-			Pipes = pipes;
 		}
 
-		public PipeConnector(Pipe initialPipe, ConnectorSide side) : this()
+		public PipeConnector(Pipe initialConnectable, ConnectorSide side) : base(initialConnectable, side)
 		{
-			Pipes = (side == ConnectorSide.A ? (initialPipe, null) : (null, initialPipe));
-
-			VacantSide = (side == ConnectorSide.A ? ConnectorSide.B : ConnectorSide.A);
 		}
 
-		private PipeConnector()
-		{
-			informationProvider = new PipeConnectorInformationProvider(this);
-
-			MainGame.DebugRender.AddDebugObjects += AddDebugObjects;
-		}
-
-		public void AddDebugObjects()
-		{
-			if (!MainGame.DebugSettings.AccessSetting<BooleanSetting>($"{nameof(PipeConnector)} debug objects"))
-				return;
-
-			MainGame.DebugRender.DrawOrientationMarks(PhysicsObject.Transform);
-
-			MainGame.DebugRender.DrawCube(new (PhysicsObject.Transform) { Scale = new((FixedDecimalInt4).4f, (FixedDecimalInt4).4f, (FixedDecimalInt4).25f)}, VacantSide is null ? RgbaFloat.Green : RgbaFloat.Cyan);
-		}
-
-		public (Pipe? pipeA, Pipe? pipeB) Pipes;
-
-		public PhysicsObject PhysicsObject;
-
-		public ConnectorProxy Proxy;
-
-		public Transform Transform;
-
-		public bool Vacant => VacantSide is not null;
-
-		public Pipe? UnconnectedPipe
-		{
-			get
-			{
-				if (!VacantSide.HasValue)
-				{
-					return null;
-				}
-
-				return (VacantSide == ConnectorSide.A ? Pipes.pipeB : Pipes.pipeA);
-			}
-		}
-
-		public ConnectorSide? VacantSide;
-
-		public ConnectorSide? PopulatedSide
-		{
-			get
-			{
-				if (!VacantSide.HasValue)
-				{
-					return null;
-				}
-
-				return (VacantSide == ConnectorSide.A ? ConnectorSide.B : ConnectorSide.A);
-			}
-		}
-
-		public void Connect(Pipe pipe)
-		{
-			if (!Vacant)
-			{
-				throw new Exception("PipeConnector is not vacant.");
-			}
-
-			if (VacantSide == ConnectorSide.A)
-			{
-				Pipes = (pipe, Pipes.pipeB);
-
-				if (Pipes.pipeB is not null)
-				{
-					VacantSide = null;
-				}
-			}
-			else if (VacantSide == ConnectorSide.B)
-			{
-				Pipes = (Pipes.pipeA, pipe);
-
-				if (Pipes.pipeA is not null)
-				{
-					VacantSide = null;
-				}
-			}
-		}
-
-		public void Disconnect(ConnectorSide side)
-		{
-			if (side == ConnectorSide.A)
-			{
-				Pipes = (null, Pipes.pipeB);
-			}
-			else if (side == ConnectorSide.B)
-			{
-				Pipes = (Pipes.pipeA, null);
-			}
-			VacantSide = side;
-
-			if (Pipes.pipeA is null && Pipes.pipeB is null)
-			{
-				PhysicsObject.Destroy();
-
-				Proxy.PhysicsObject.Destroy();
-
-				MainGame.DebugRender.AddDebugObjects -= AddDebugObjects;
-			}
-		}
-
-		public void Disconnect(Pipe pipe)
-		{
-			if (Pipes.pipeA == pipe)
-			{
-				Disconnect(ConnectorSide.A);
-			}
-			else if (Pipes.pipeB == pipe)
-			{
-				Disconnect(ConnectorSide.B);
-			}
-		}
+		public (Pipe? pipeA, Pipe? pipeB) Pipes => ((Pipe? pipeA, Pipe? pipeB))Connectables;
 
 		private IInformationProvider informationProvider;
 

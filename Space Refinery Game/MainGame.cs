8a808ae0bba5a30515a1c103fff5ceb48a561af2
Pixxl <oliverhalenius@yahoo.se>
2022-04-19 16:55:35 +0200

    remove redundant LightInfo struct

--- Space Refinery Game/MainGame.cs
+++ Space Refinery Game/MainGame.cs
@@ -247,21 +247,4 @@ public class MainGame
 			}
 		}
 	}
-
-	[StructLayout(LayoutKind.Sequential)]
-	public struct LightInfo
-	{
-		public BlittableVector3 LightDirection;
-		private float padding0;
-		public BlittableVector3 CameraPosition;
-		private float padding1;
-
-		public LightInfo(Vector3 lightDirection, Vector3 cameraPosition)
-		{
-			LightDirection = new BlittableVector3(lightDirection);
-			CameraPosition = new BlittableVector3(cameraPosition);
-			padding0 = 0;
-			padding1 = 0;
-		}
-	}
 }

bb8dcd9e14a40e0b2f9d85df2836d07a3a633d53
Pixxl <oliverhalenius@yahoo.se>
2022-04-29 00:51:47 +0200

    add UIFunctions

---
+++ Space Refinery Game/UIFunctions.cs
@@ -0,0 +1,40 @@
+﻿using ImGuiNET;
+
+namespace Space_Refinery_Game
+{
+	public static class UIFunctions
+	{
+		public static void DoSelector<T>(ICollection<T> selectables, ref int selection, out bool hasSelection, out T selected) where T : IUIInspectable
+		{
+			hasSelection = false;
+
+			selected = default;
+
+			ImGui.BeginGroup();
+			{
+				int selectionIndex = 0;
+				foreach (T selectable in selectables)
+				{
+					selectable.DoUIInspectorReadonly();
+
+					if (ImGui.Selectable($"Select {selectionIndex}", selectionIndex == selection) || selectionIndex == selection)
+					{
+						selection = selectionIndex;
+
+						hasSelection = true;
+
+						selected = selectable;
+					}
+
+					selectionIndex++;
+				}			
+			}
+			ImGui.EndGroup();
+
+			if (!hasSelection)
+			{
+				selection = -1;
+			}
+		}
+	}
+}

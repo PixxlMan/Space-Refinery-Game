4b4d0cbf55ad5e5015c3bda32b5f1fcf1417bccf
Pixxl <oliverhalenius@yahoo.se>
2022-04-30 01:17:22 +0200

    add DoListManipulation and BeginSub and EndSub

--- Space Refinery Game/UIFunctions.cs
+++ Space Refinery Game/UIFunctions.cs
@@ -4,6 +4,20 @@ namespace Space_Refinery_Game
 {
 	public static class UIFunctions
 	{
+		public static void BeginSub()
+		{
+			ImGui.PushID(0);
+			ImGui.BeginGroup();
+			ImGui.Indent();
+		}
+
+		public static void EndSub()
+		{
+			ImGui.Unindent();
+			ImGui.EndGroup();
+			ImGui.PopID();
+		}
+
 		public static void DoSelector<T>(ICollection<T> selectables, ref int selection, out bool hasSelection, out T selected) where T : IUIInspectable
 		{
 			hasSelection = false;
@@ -35,6 +49,40 @@ namespace Space_Refinery_Game
 			{
 				selection = -1;
 			}
+		}
+
+		public static void DoListManipulation<T>(IList<T> collection, bool allowEditingItems) where T : IUIInspectable
+		{
+			List<int> indexesToRemove = new();
+
+			ImGui.Indent();
+			{
+				int currentIndex = 0;
+				foreach (var item in collection)
+				{
+					if (allowEditingItems)
+					{
+						item.DoUIInspectorEditable();
+					}
+					else
+					{
+						item.DoUIInspectorReadonly();
+					}
+
+					if (ImGui.Button("Delete"))
+					{
+						indexesToRemove.Add(currentIndex);
+					}
+
+					currentIndex++;
+				}
+
+				foreach (var index in indexesToRemove)
+				{
+					collection.RemoveAt(index);
+				}
+			}
+			ImGui.Unindent();
 		}
 	}
 }

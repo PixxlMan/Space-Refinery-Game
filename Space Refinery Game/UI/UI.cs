4af6d2afd602f4bcbec8764d40d36b21b5206247
Pixxl <oliverhalenius@yahoo.se>
2022-04-30 17:32:23 +0200

    add fonts and test styling

--- Space Refinery Game/UI/UI.cs
+++ Space Refinery Game/UI/UI.cs
@@ -113,6 +113,8 @@ namespace Space_Refinery_Game
 
 			ui.PipeTypes.AddRange(PipeType.GetAllPipeTypes(graphWorld));
 
+			ui.Style();
+
 			return ui;
 		}
 
@@ -347,6 +349,79 @@ namespace Space_Refinery_Game
 				}
 			}
 			ImGui.End();
+		}
+
+		public void Style() // https://github.com/ocornut/imgui/issues/707
+		{
+			ImGuiStylePtr style = ImGui.GetStyle();
+			var colors = style.Colors;
+
+			/// 0 = FLAT APPEARENCE
+			/// 1 = MORE "3D" LOOK
+			int is3D = 1;
+
+			colors[(int)ImGuiCol.Text] = new(1.00f, 1.00f, 1.00f, 1.00f);
+			colors[(int)ImGuiCol.TextDisabled] = new(0.40f, 0.40f, 0.40f, 1.00f);
+			colors[(int)ImGuiCol.ChildBg] = new(0.25f, 0.25f, 0.25f, 1.00f);
+			colors[(int)ImGuiCol.WindowBg] = new(0.25f, 0.25f, 0.25f, 1.00f);
+			colors[(int)ImGuiCol.PopupBg] = new(0.25f, 0.25f, 0.25f, 1.00f);
+			colors[(int)ImGuiCol.Border] = new(0.12f, 0.12f, 0.12f, 0.71f);
+			colors[(int)ImGuiCol.BorderShadow] = new(1.00f, 1.00f, 1.00f, 0.06f);
+			colors[(int)ImGuiCol.FrameBg] = new(0.42f, 0.42f, 0.42f, 0.54f);
+			colors[(int)ImGuiCol.FrameBgHovered] = new(0.42f, 0.42f, 0.42f, 0.40f);
+			colors[(int)ImGuiCol.FrameBgActive] = new(0.56f, 0.56f, 0.56f, 0.67f);
+			colors[(int)ImGuiCol.TitleBg] = new(0.19f, 0.19f, 0.19f, 1.00f);
+			colors[(int)ImGuiCol.TitleBgActive] = new(0.22f, 0.22f, 0.22f, 1.00f);
+			colors[(int)ImGuiCol.TitleBgCollapsed] = new(0.17f, 0.17f, 0.17f, 0.90f);
+			colors[(int)ImGuiCol.MenuBarBg] = new(0.335f, 0.335f, 0.335f, 1.000f);
+			colors[(int)ImGuiCol.ScrollbarBg] = new(0.24f, 0.24f, 0.24f, 0.53f);
+			colors[(int)ImGuiCol.ScrollbarGrab] = new(0.41f, 0.41f, 0.41f, 1.00f);
+			colors[(int)ImGuiCol.ScrollbarGrabHovered] = new(0.52f, 0.52f, 0.52f, 1.00f);
+			colors[(int)ImGuiCol.ScrollbarGrabActive] = new(0.76f, 0.76f, 0.76f, 1.00f);
+			colors[(int)ImGuiCol.CheckMark] = new(0.65f, 0.65f, 0.65f, 1.00f);
+			colors[(int)ImGuiCol.SliderGrab] = new(0.52f, 0.52f, 0.52f, 1.00f);
+			colors[(int)ImGuiCol.SliderGrabActive] = new(0.64f, 0.64f, 0.64f, 1.00f);
+			colors[(int)ImGuiCol.Button] = new(0.54f, 0.54f, 0.54f, 0.35f);
+			colors[(int)ImGuiCol.ButtonHovered] = new(0.52f, 0.52f, 0.52f, 0.59f);
+			colors[(int)ImGuiCol.ButtonActive] = new(0.76f, 0.76f, 0.76f, 1.00f);
+			colors[(int)ImGuiCol.Header] = new(0.38f, 0.38f, 0.38f, 1.00f);
+			colors[(int)ImGuiCol.HeaderHovered] = new(0.47f, 0.47f, 0.47f, 1.00f);
+			colors[(int)ImGuiCol.HeaderActive] = new(0.76f, 0.76f, 0.76f, 0.77f);
+			colors[(int)ImGuiCol.Separator] = new(0.000f, 0.000f, 0.000f, 0.137f);
+			colors[(int)ImGuiCol.SeparatorHovered] = new(0.700f, 0.671f, 0.600f, 0.290f);
+			colors[(int)ImGuiCol.SeparatorActive] = new(0.702f, 0.671f, 0.600f, 0.674f);
+			colors[(int)ImGuiCol.ResizeGrip] = new(0.26f, 0.59f, 0.98f, 0.25f);
+			colors[(int)ImGuiCol.ResizeGripHovered] = new(0.26f, 0.59f, 0.98f, 0.67f);
+			colors[(int)ImGuiCol.ResizeGripActive] = new(0.26f, 0.59f, 0.98f, 0.95f);
+			colors[(int)ImGuiCol.PlotLines] = new(0.61f, 0.61f, 0.61f, 1.00f);
+			colors[(int)ImGuiCol.PlotLinesHovered] = new(1.00f, 0.43f, 0.35f, 1.00f);
+			colors[(int)ImGuiCol.PlotHistogram] = new(0.90f, 0.70f, 0.00f, 1.00f);
+			colors[(int)ImGuiCol.PlotHistogramHovered] = new(1.00f, 0.60f, 0.00f, 1.00f);
+			colors[(int)ImGuiCol.TextSelectedBg] = new(0.73f, 0.73f, 0.73f, 0.35f);
+			colors[(int)ImGuiCol.ModalWindowDimBg] = new(0.80f, 0.80f, 0.80f, 0.35f);
+			colors[(int)ImGuiCol.DragDropTarget] = new(1.00f, 1.00f, 0.00f, 0.90f);
+			colors[(int)ImGuiCol.NavHighlight] = new(0.26f, 0.59f, 0.98f, 1.00f);
+			colors[(int)ImGuiCol.NavWindowingHighlight] = new(1.00f, 1.00f, 1.00f, 0.70f);
+			colors[(int)ImGuiCol.NavWindowingDimBg] = new(0.80f, 0.80f, 0.80f, 0.20f);
+
+			style.PopupRounding = 3;
+
+			style.WindowPadding = new(4, 4);
+			style.FramePadding = new(6, 4);
+			style.ItemSpacing = new(6, 2);
+
+			style.ScrollbarSize = 18;
+
+			style.WindowBorderSize = 1;
+			style.ChildBorderSize = 1;
+			style.PopupBorderSize = 1;
+			style.FrameBorderSize = is3D;
+
+			style.WindowRounding = 3;
+			style.ChildRounding = 3;
+			style.FrameRounding = 3;
+			style.ScrollbarRounding = 2;
+			style.GrabRounding = 3;
 		}
 	}
 }

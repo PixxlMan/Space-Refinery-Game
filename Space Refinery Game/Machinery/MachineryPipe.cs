c65ae49dcb8821097872f8f886404175d9c0b321
Pixxl <oliverhalenius@yahoo.se>
2022-05-07 00:53:15 +0200

    add MachineryPipe

---
+++ Space Refinery Game/Machinery/MachineryPipe.cs
@@ -0,0 +1,63 @@
+﻿using FixedPrecision;
+using ImGuiNET;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Space_Refinery_Game
+{
+	public abstract class MachineryPipe : Pipe
+	{
+		protected MachineryPipe()
+		{
+			informationProvider = new MachineryPipeInformationProvider(this);
+		}
+
+		public Dictionary<PipeConnector, ResourceContainer> ResourceContainers = new();
+
+		public bool Activated;
+
+		public override ResourceContainer GetResourceContainerForConnector(PipeConnector pipeConnector)
+		{
+			lock (this)
+			{
+				return ResourceContainers[pipeConnector];
+			}
+		}
+
+		public override void TransferResourceFromConnector(ResourceContainer source, FixedDecimalLong8 volume, Connector transferingConnector)
+		{
+			lock (this)
+			{
+				source.TransferResource(ResourceContainers[(PipeConnector)transferingConnector], volume);
+			}
+		}
+
+		protected override void DisplaceContents()
+		{
+			throw new NotImplementedException();
+		}
+
+		protected override void SetUp()
+		{
+
+		}
+
+		protected override void Tick()
+		{
+
+		}
+
+		protected override void Interacted()
+		{
+			UI.EnterMenu(DoMenu, "Machinery");
+		}
+
+		protected virtual void DoMenu()
+		{
+			ImGui.Checkbox("Powered", ref Activated);
+		}
+	}
+}
---
+++ Space Refinery Game/Machinery/MachineryPipeInformationProvider.cs
@@ -0,0 +1,23 @@
+﻿using ImGuiNET;
+
+namespace Space_Refinery_Game
+{
+	public class MachineryPipeInformationProvider : PipeInformationProvider
+	{
+		public MachineryPipeInformationProvider(MachineryPipe pipe) : base(pipe)
+		{
+		}
+
+		public override void InformationUI()
+		{
+			base.InformationUI();
+
+			var machineryPipe = (MachineryPipe)Pipe;
+
+			foreach (var resourceContainer in machineryPipe.ResourceContainers.Values)
+			{
+				ImGui.Text(resourceContainer.ToString());
+			}
+		}
+	}
+}

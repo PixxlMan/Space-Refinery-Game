8d89cfd45b8a681e26a3fc0c117a5d4995e89fe5
Pixxl <oliverhalenius@yahoo.se>
2022-09-29 21:28:13 +0200

    add GameData type

---
+++ Space Refinery Game/Connectors/GameData.cs
@@ -0,0 +1,29 @@
+﻿using Space_Refinery_Game_Renderer;
+
+namespace Space_Refinery_Game
+{
+	public class GameData
+	{
+		public GameData(UI ui, PhysicsWorld physicsWorld, GraphicsWorld graphicsWorld, GameWorld gameWorld, MainGame mainGame, SerializationReferenceHandler referenceHandler)
+		{
+			UI = ui;
+			PhysicsWorld = physicsWorld;
+			GraphicsWorld = graphicsWorld;
+			GameWorld = gameWorld;
+			MainGame = mainGame;
+			ReferenceHandler = referenceHandler;
+		}
+
+		public UI UI { get; }
+
+		public PhysicsWorld PhysicsWorld { get; }
+
+		public GraphicsWorld GraphicsWorld { get; }
+
+		public GameWorld GameWorld { get; }
+
+		public MainGame MainGame { get; }
+
+		public SerializationReferenceHandler ReferenceHandler { get; }
+	}
+}

d7e3aba8843e146425cf5d90e529d39d9d644bce
Pixxl <oliverhalenius@yahoo.se>
2022-10-06 00:54:18 +0200

    make EntityRenderable handle it's own lifetime on the GraphicsWorld

--- Space Refinery Game/Pipes/ValvePipe.cs
+++ Space Refinery Game/Pipes/ValvePipe.cs
@@ -45,7 +45,7 @@ namespace Space_Refinery_Game
 					InternalBlockerModel = GraphicsWorld.MeshLoader.LoadCached(Path.Combine(Environment.CurrentDirectory, "Assets", "Models", "Pipe", "Special", "PipeSpecialValveInternalBlocker.obj"));
 				}
 
-				InternalBlockerRenderable = EntityRenderable.Create(GraphicsWorld.ShaderLoader, GraphicsWorld.GraphicsDevice, GraphicsWorld.Factory, Transform, InternalBlockerModel, Utils.GetSolidColoredTexture(RgbaByte.LightGrey, GraphicsWorld.GraphicsDevice, GraphicsWorld.Factory), GraphicsWorld.CameraProjViewBuffer, GraphicsWorld.LightInfoBuffer);
+				InternalBlockerRenderable = EntityRenderable.Create(GraphicsWorld, Transform, InternalBlockerModel, Utils.GetSolidColoredTexture(RgbaByte.LightGrey, GraphicsWorld.GraphicsDevice, GraphicsWorld.Factory), GraphicsWorld.CameraProjViewBuffer, GraphicsWorld.LightInfoBuffer);
 
 				GraphicsWorld.AddRenderable(InternalBlockerRenderable);
 
@@ -120,8 +120,6 @@ namespace Space_Refinery_Game
 				base.Deconstruct();
 
 				InternalBlockerRenderable.Destroy();
-
-				GraphicsWorld.RemoveRenderable(InternalBlockerRenderable);
 			}
 		}
 

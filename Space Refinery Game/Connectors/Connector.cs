96a60855df90f06c692bacae805b6f7baa07fcc2
Pixxl <oliverhalenius@yahoo.se>
2022-10-06 19:07:51 +0200

    add locking for better thread safety

--- Space Refinery Game/Connectors/Connector.cs
+++ Space Refinery Game/Connectors/Connector.cs
@@ -62,21 +62,26 @@ namespace Space_Refinery_Game
 
 		public PhysicsObject PhysicsObject;
 
+		public object SyncRoot = new();
+
 		public ConnectorSide? VacantSide
 		{
 			get
 			{
-				if (Connectables.connectableA is null)
+				lock (SyncRoot)
 				{
-					return ConnectorSide.A;
-				}
-				else if (Connectables.connectableB is null)
-				{
-					return ConnectorSide.B;
-				}
-				else
-				{
-					return null;
+					if (Connectables.connectableA is null)
+					{
+						return ConnectorSide.A;
+					}
+					else if (Connectables.connectableB is null)
+					{
+						return ConnectorSide.B;
+					}
+					else
+					{
+						return null;
+					}
 				}
 			}
 		}
@@ -93,229 +98,268 @@ namespace Space_Refinery_Game
 		{
 			get
 			{
-				if (!VacantSide.HasValue)
+				lock (SyncRoot)
 				{
-					return null;
-				}
+					if (!VacantSide.HasValue)
+					{
+						return null;
+					}
 
-				return (VacantSide == ConnectorSide.A ? Connectables.connectableB : Connectables.connectableA);
+					return (VacantSide == ConnectorSide.A ? Connectables.connectableB : Connectables.connectableA);
+				}
 			}
 		}
 
 		public IConnectable? GetConnectableAtSide(ConnectorSide side)
 		{
-			return side == ConnectorSide.A ? Connectables.connectableA : Connectables.connectableB;
+			lock (SyncRoot)
+			{
+				return side == ConnectorSide.A ? Connectables.connectableA : Connectables.connectableB;
+			}
 		}
 
 		public IConnectable? GetOther(IConnectable connectable)
 		{
-			if (connectable == Connectables.connectableA)
+			lock (SyncRoot)
 			{
-				return Connectables.connectableB;
-			}
-			else if (connectable == Connectables.connectableB)
-			{
-				return Connectables.connectableA;
-			}
-			else
-			{
-				throw new ArgumentException("Connectable is not present on connector.", nameof(connectable));
+				if (connectable == Connectables.connectableA)
+				{
+					return Connectables.connectableB;
+				}
+				else if (connectable == Connectables.connectableB)
+				{
+					return Connectables.connectableA;
+				}
+				else
+				{
+					throw new ArgumentException("Connectable is not present on connector.", nameof(connectable));
+				}
 			}
 		}
 
 		public ConnectorSide GetConnectorSide(IConnectable connectable)
 		{
-			if (Connectables.connectableA == connectable)
+			lock (SyncRoot)
 			{
-				return ConnectorSide.A;
-			}
-			else if (Connectables.connectableB == connectable)
-			{
-				return ConnectorSide.B;
-			}
-			else
-			{
-				throw new ArgumentException("Connectable is not present on connector.", nameof(connectable));
+				if (Connectables.connectableA == connectable)
+				{
+					return ConnectorSide.A;
+				}
+				else if (Connectables.connectableB == connectable)
+				{
+					return ConnectorSide.B;
+				}
+				else
+				{
+					throw new ArgumentException("Connectable is not present on connector.", nameof(connectable));
+				}
 			}
 		}
 
 		public void Connect(IConnectable connectable)
 		{
-			if (!Vacant)
+			lock (SyncRoot)
 			{
-				throw new Exception($"{nameof(Connector)} is not vacant.");
-			}
+				if (!Vacant)
+				{
+					throw new Exception($"{nameof(Connector)} is not vacant.");
+				}
 
-			if (VacantSide == ConnectorSide.A)
-			{
-				Connectables = (connectable, Connectables.connectableB);
-			}
-			else if (VacantSide == ConnectorSide.B)
-			{
-				Connectables = (Connectables.connectableA, connectable);
-			}
+				if (VacantSide == ConnectorSide.A)
+				{
+					Connectables = (connectable, Connectables.connectableB);
+				}
+				else if (VacantSide == ConnectorSide.B)
+				{
+					Connectables = (Connectables.connectableA, connectable);
+				}
 
-			PhysicsObject.Enabled = false;
-			Proxy.Disable();
+				PhysicsObject.Enabled = false;
+				Proxy.Disable(); 
+			}
 		}
 
 		public void Disconnect(ConnectorSide side)
 		{
-			if (side == ConnectorSide.A)
+			lock (SyncRoot)
 			{
-				Connectables = (null, Connectables.connectableB);
-			}
-			else if (side == ConnectorSide.B)
-			{
-				Connectables = (Connectables.connectableA, null);
-			}
+				if (side == ConnectorSide.A)
+				{
+					Connectables = (null, Connectables.connectableB);
+				}
+				else if (side == ConnectorSide.B)
+				{
+					Connectables = (Connectables.connectableA, null);
+				}
 
-			PhysicsObject.Enabled = true;
-			UpdateProxy();
+				PhysicsObject.Enabled = true;
+				UpdateProxy();
 
-			if (Connectables.connectableA is null && Connectables.connectableB is null)
-			{
-				Destroy();
+				if (Connectables.connectableA is null && Connectables.connectableB is null)
+				{
+					Destroy();
+				} 
 			}
 		}
 
 		public void Disconnect(IConnectable connectable)
 		{
-			if (Connectables.connectableA == connectable)
+			lock (SyncRoot)
 			{
-				Disconnect(ConnectorSide.A);
-			}
-			else if (Connectables.connectableB == connectable)
-			{
-				Disconnect(ConnectorSide.B);
+				if (Connectables.connectableA == connectable)
+				{
+					Disconnect(ConnectorSide.A);
+				}
+				else if (Connectables.connectableB == connectable)
+				{
+					Disconnect(ConnectorSide.B);
+				} 
 			}
 		}
 
 		public void UpdateProxy()
 		{
-			if (!Vacant)
+			lock (SyncRoot)
 			{
-				return;
+				if (!Vacant)
+				{
+					return;
+				}
+
+				if (Proxy is null)
+				{
+					Proxy = new(this);
+				}
+
+				if (Proxy.PhysicsObject is not null)
+				{
+					Proxy.PhysicsObject.Destroy();
+				}
+
+				var proxyPhysicsObject = new PhysicsObjectDescription<ConvexHull>(gameData.PhysicsWorld.GetConvexHullForMesh(gameData.UI.SelectedPipeType.Mesh), GameWorld.GenerateTransformForConnector(gameData.UI.SelectedPipeType.ConnectorPlacements[gameData.UI.ConnectorSelection], this, gameData.UI.RotationIndex * 45 * FixedDecimalLong8.DegreesToRadians), 0, true);
+
+				Proxy.PhysicsObject = gameData.PhysicsWorld.AddPhysicsObject(proxyPhysicsObject, Proxy);
+
+				Proxy.Enable(); 
 			}
-
-			if (Proxy is null)
-			{
-				Proxy = new(this);
-			}
-
-			if (Proxy.PhysicsObject is not null)
-			{
-				Proxy.PhysicsObject.Destroy();
-			}
-
-			var proxyPhysicsObject = new PhysicsObjectDescription<ConvexHull>(gameData.PhysicsWorld.GetConvexHullForMesh(gameData.UI.SelectedPipeType.Mesh), GameWorld.GenerateTransformForConnector(gameData.UI.SelectedPipeType.ConnectorPlacements[gameData.UI.ConnectorSelection], this, gameData.UI.RotationIndex * 45 * FixedDecimalLong8.DegreesToRadians), 0, true);
-
-			Proxy.PhysicsObject = gameData.PhysicsWorld.AddPhysicsObject(proxyPhysicsObject, Proxy);
-
-			Proxy.Enable();
 		}
 
 		public ConnectorSide? PopulatedSide
 		{
 			get
 			{
-				if (!VacantSide.HasValue)
+				lock (SyncRoot)
 				{
-					return null;
-				}
+					if (!VacantSide.HasValue)
+					{
+						return null;
+					}
 
-				return VacantSide.Value.Opposite();
+					return VacantSide.Value.Opposite(); 
+				}
 			}
 		}
 
 		private void AddDebugObjects()
 		{
-			if (!MainGame.DebugSettings.AccessSetting<BooleanDebugSetting>($"{nameof(Connector)} debug objects"))
-				return;
+			lock (SyncRoot)
+			{
+				if (!MainGame.DebugSettings.AccessSetting<BooleanDebugSetting>($"{nameof(Connector)} debug objects"))
+					return;
 
-			MainGame.DebugRender.DrawOrientationMarks(PhysicsObject.Transform);
+				MainGame.DebugRender.DrawOrientationMarks(PhysicsObject.Transform);
 
-			MainGame.DebugRender.DrawCube(new Transform(PhysicsObject.Transform) { Scale = new((FixedDecimalInt4).4f, (FixedDecimalInt4).4f, (FixedDecimalInt4).25f) }, VacantSide is null ? RgbaFloat.Green : RgbaFloat.Cyan);
+				MainGame.DebugRender.DrawCube(new Transform(PhysicsObject.Transform) { Scale = new((FixedDecimalInt4).4f, (FixedDecimalInt4).4f, (FixedDecimalInt4).25f) }, VacantSide is null ? RgbaFloat.Green : RgbaFloat.Cyan); 
+			}
 		}
 
 		public virtual void SerializeState(XmlWriter writer)
 		{
-			writer.WriteStartElement(nameof(Connector));
+			lock (SyncRoot)
 			{
-				writer.SerializeReference(this);
-
-				writer.Serialize(Transform);
-
-				writer.Serialize(Connectables.connectableA is not null, "HasA");
-				writer.Serialize(Connectables.connectableB is not null, "HasB");
-
-				if (Connectables.connectableA is not null)
+				writer.WriteStartElement(nameof(Connector));
 				{
-					writer.SerializeReference(Connectables.connectableA, $"{nameof(Connectables.connectableA)}_GUID");
-				}
+					writer.SerializeReference(this);
 
-				if (Connectables.connectableB is not null)
-				{
-					writer.SerializeReference(Connectables.connectableB, $"{nameof(Connectables.connectableB)}_GUID");
+					writer.Serialize(Transform);
+
+					writer.Serialize(Connectables.connectableA is not null, "HasA");
+					writer.Serialize(Connectables.connectableB is not null, "HasB");
+
+					if (Connectables.connectableA is not null)
+					{
+						writer.SerializeReference(Connectables.connectableA, $"{nameof(Connectables.connectableA)}_GUID");
+					}
+
+					if (Connectables.connectableB is not null)
+					{
+						writer.SerializeReference(Connectables.connectableB, $"{nameof(Connectables.connectableB)}_GUID");
+					}
 				}
+				writer.WriteEndElement(); 
 			}
-			writer.WriteEndElement();
 		}
 
 		public virtual void DeserializeState(XmlReader reader, SerializationData serializationData, SerializationReferenceHandler referenceHandler)
 		{
-			reader.ReadStartElement(nameof(Connector));
+			lock (SyncRoot)
 			{
-				SerializableReferenceGUID = reader.ReadReferenceGUID();
-
-				Transform = reader.DeserializeTransform();
-
-				IConnectable a = null, b = null;
-				bool hasA = reader.DeserializeBoolean("HasA");
-				bool hasB = reader.DeserializeBoolean("HasB");
-
-				if (hasA)
+				reader.ReadStartElement(nameof(Connector));
 				{
-					reader.DeserializeReference<IConnectable>(referenceHandler, (es) => a = (IConnectable)es, $"{nameof(Connectables.connectableA)}_GUID");
+					SerializableReferenceGUID = reader.ReadReferenceGUID();
+
+					Transform = reader.DeserializeTransform();
+
+					IConnectable a = null, b = null;
+					bool hasA = reader.DeserializeBoolean("HasA");
+					bool hasB = reader.DeserializeBoolean("HasB");
+
+					if (hasA)
+					{
+						reader.DeserializeReference<IConnectable>(referenceHandler, (es) => a = (IConnectable)es, $"{nameof(Connectables.connectableA)}_GUID");
+					}
+
+					if (hasB)
+					{
+						reader.DeserializeReference<IConnectable>(referenceHandler, (es) => b = (IConnectable)es, $"{nameof(Connectables.connectableB)}_GUID");
+					}
+
+					serializationData.SerializationCompleteEvent += () =>
+					{
+						Connectables = (a, b);
+					};
+
+					this.gameData = serializationData.GameData;
+
+					serializationData.GameData.GameWorld.AddEntity(this);
 				}
-
-				if (hasB)
-				{
-					reader.DeserializeReference<IConnectable>(referenceHandler, (es) => b = (IConnectable)es, $"{nameof(Connectables.connectableB)}_GUID");
-				}
-
-				serializationData.SerializationCompleteEvent += () =>
-				{
-					Connectables = (a, b);
-				};
-
-				this.gameData = serializationData.GameData;
-
-				serializationData.GameData.GameWorld.AddEntity(this);
+				reader.ReadEndElement(); 
 			}
-			reader.ReadEndElement();
 		}
 
 		public virtual void Destroy()
 		{
-			if (Destroyed)
+			lock (SyncRoot)
 			{
-				return;
+				if (Destroyed)
+				{
+					return;
+				}
+
+				Destroyed = true;
+
+				PhysicsObject.Destroy();
+
+				Proxy.PhysicsObject.Destroy();
+
+				gameData.UI.SelectedEntityTypeChanged -= UpdateProxyOnEntityTypeChanged;
+
+				MainGame.DebugRender.AddDebugObjects -= AddDebugObjects;
+
+				gameData.GameWorld.RemoveEntity(this);
+
+				gameData.ReferenceHandler.RemoveReference(this); 
 			}
-
-			Destroyed = true;
-
-			PhysicsObject.Destroy();
-
-			Proxy.PhysicsObject.Destroy();
-
-			gameData.UI.SelectedEntityTypeChanged -= UpdateProxyOnEntityTypeChanged;
-
-			MainGame.DebugRender.AddDebugObjects -= AddDebugObjects;
-
-			gameData.GameWorld.RemoveEntity(this);
-
-			gameData.ReferenceHandler.RemoveReference(this);
 		}
 
 		public abstract void Tick();

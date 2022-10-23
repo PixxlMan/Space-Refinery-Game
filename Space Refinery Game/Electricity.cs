5101fea5df3bd8f1917bf98c26f423aed44c3e7e
Pixxl <oliverhalenius@yahoo.se>
2022-10-23 19:01:32 +0200

    add Electricity

---
+++ Space Refinery Game/Electricity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Space_Refinery_Game
+{
+	public static class Electricity
+	{
+		public static DecimalNumber Voltage => 1000; // V or J/C
+
+		public static DecimalNumber ElectricalEnergyToCoulomb(DecimalNumber electricalEnergy /* J */)
+		{
+			return electricalEnergy * Voltage; // J * J/C => C
+		}
+	}
+}

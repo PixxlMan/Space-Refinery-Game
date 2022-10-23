bfccb13ba70bd501dac12dac3507ca442208dc4b
Pixxl <oliverhalenius@yahoo.se>
2022-10-23 18:59:02 +0200

    add CanOccurSpontaneously and GetAllPossibleReactionTypes

--- Space Refinery Game/ReactionType.cs
+++ Space Refinery Game/ReactionType.cs
@@ -13,15 +13,59 @@ namespace Space_Refinery_Game
 		public ReactionType()
 		{
 			ReactionTypes.Add(this);
+
+			if (CanOccurSpontaneously)
+			{
+				foreach (ChemicalType chemicalType in NecessaryChemicals)
+				{
+					if (PossibleReactionsPerChemicalType.ContainsKey(chemicalType))
+					{
+						PossibleReactionsPerChemicalType[chemicalType].Add(this);
+					}
+					else
+					{
+						PossibleReactionsPerChemicalType.TryAdd(chemicalType, new() { this });
+					}
+				}
+			}
+		}
+
+		public ICollection<ReactionType> GetAllPossibleReactionTypes(HashSet<ChemicalType> availableChemicals)
+		{
+			HashSet<ReactionType> initialPossibleReactions = new();
+
+			foreach (var chemical in availableChemicals)
+			{
+				if (PossibleReactionsPerChemicalType.ContainsKey(chemical))
+				{
+					initialPossibleReactions.UnionWith(PossibleReactionsPerChemicalType[chemical]);
+				}
+			}
+
+			HashSet<ReactionType> refinedPossibleReactions = new();
+
+			foreach (var possibleReaction in initialPossibleReactions)
+			{
+				if (possibleReaction.NecessaryChemicals.IsSubsetOf(availableChemicals))
+				{
+					refinedPossibleReactions.Add(possibleReaction);
+				}
+			}
+
+			return refinedPossibleReactions;
 		}
 
 		public static List<ReactionType> ReactionTypes = new();
 
+		public ConcurrentDictionary<ChemicalType, HashSet<ReactionType>> PossibleReactionsPerChemicalType = new();
+
 		public string ReactionName { get; protected set; }
 
-		public ICollection<ChemicalType> NecessaryChemicals { get; private set; }
+		public HashSet<ChemicalType> NecessaryChemicals { get; private set; }
 
 		public Guid SerializableReferenceGUID { get; private set; }
+
+		public bool CanOccurSpontaneously { get; protected set; }
 
 		public abstract void Tick(DecimalNumber interval, ResourceContainer resourceContainer);
 
@@ -32,6 +76,8 @@ namespace Space_Refinery_Game
 				writer.SerializeReference(this);
 
 				writer.Serialize(ReactionName, nameof(ReactionName));
+
+				writer.Serialize(CanOccurSpontaneously, nameof(CanOccurSpontaneously));
 
 				writer.Serialize(NecessaryChemicals, nameof(NecessaryChemicals));
 			}
@@ -46,12 +92,14 @@ namespace Space_Refinery_Game
 
 				ReactionName = reader.ReadString(nameof(ReactionName));
 
+				CanOccurSpontaneously = reader.DeserializeBoolean(nameof(CanOccurSpontaneously));
+
 				ConcurrentBag<ChemicalType> necessaryChemicalTypes = new();
 				reader.DeserializeReferenceCollection(necessaryChemicalTypes, referenceHandler, nameof(NecessaryChemicals));
 
 				serializationData.SerializationCompleteEvent += () =>
 				{
-					NecessaryChemicals = necessaryChemicalTypes.ToArray();
+					NecessaryChemicals = necessaryChemicalTypes.ToHashSet();
 				};
 			}
 			reader.ReadEndElement();

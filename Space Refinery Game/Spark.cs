4f79deb5af6f64e2be06d7cd2d6b5ee08dd3154c
Pixxl <oliverhalenius@yahoo.se>
2022-10-29 02:51:51 +0200

    add HydrogenOxygenCombustionReactionType and Spark and Fire ReactionFactors

---
+++ Space Refinery Game/Spark.cs
@@ -0,0 +1,21 @@
+﻿namespace Space_Refinery_Game
+{
+	public struct Spark : ReactionFactor
+	{
+		/// <summary>
+		/// [J]
+		/// </summary>
+		public DecimalNumber SparkEnergy;
+
+		public Spark(DecimalNumber sparkEnergy)
+		{
+			SparkEnergy = sparkEnergy;
+		}
+	}
+
+	public struct Fire : ReactionFactor
+	{
+		public Fire()
+		{ }
+	}
+}

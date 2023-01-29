c89ae3e7f25b1f71288e08eda4af46af1e85834e
Pixxl <oliverhalenius@yahoo.se>
2023-01-29 03:31:33 +0100

    add values and ordering according to entropy to ChemicalPhase

--- Space Refinery Game/Chemicals/ChemicalPhase.cs
+++ Space Refinery Game/Chemicals/ChemicalPhase.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace Space_Refinery_Game
 {
-	public enum ChemicalPhase
+	public enum ChemicalPhase // Ordered by entropy of the phase. The larger the number the greater the entropy.
 	{
-		Solid,
-		Liquid,
-		Gas,
-		Plasma
+		Solid = 0,
+		Liquid = 1,
+		Gas = 2,
+		Plasma = 3
 	}
 }

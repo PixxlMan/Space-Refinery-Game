913ac3a03b01878a57b137d76d72d5186bd05b42
Pixxl <oliverhalenius@yahoo.se>
2023-02-17 18:29:40 +0100

    correctly position debug menu

--- Space Refinery Game/UI/Interface/PauseUI.cs
+++ Space Refinery Game/UI/Interface/PauseUI.cs
@@ -15,8 +15,10 @@ partial class UI
 	{
 		lock (syncRoot)
 		{
-			if (ImGui.Begin("Debug Settings", ImGuiWindowFlags.AlwaysAutoResize))
+			if (ImGui.Begin("Debug Settings", ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoMove))
 			{
+				ImGui.SetWindowSize(debugSettingsMenuSize, ImGuiCond.Always);
+				ImGui.SetWindowPos(new Vector2((width / 2 - pauseMenuSize.X / 2) + pauseMenuSize.X, height / 2 - debugSettingsMenuSize.Y / 2), ImGuiCond.Always);
 				foreach (var debugSetting in MainGame.DebugSettings.DebugSettingsDictionary.Values)
 				{
 					debugSetting.DrawUIElement();
@@ -27,6 +29,8 @@ partial class UI
 			}
 		}
 	}
+
+	private Vector2 debugSettingsMenuSize => new Vector2((width / 3) * 0.9f, (height / 10) * 8);
 
 	private Vector2 pauseMenuSize => new Vector2(width / 3, (height / 10) * 8);
 

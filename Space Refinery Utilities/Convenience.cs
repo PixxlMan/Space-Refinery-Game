c0ac752e69146a1b41977cdcbbd9a9bb7600f0c3
Pixxl <oliverhalenius@yahoo.se>
2023-03-21 16:45:03 +0100

    add Convenience class and add SelectRandom and SelectRandomNew

---
+++ Space Refinery Utilities/Convenience.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Space_Refinery_Utilities
+{
+	public static class Convenience
+	{
+		public static T SelectRandom<T>(this ICollection<T> collection, Random? random = null)
+		{
+			random ??= Random.Shared;
+
+			return collection.ElementAt(random.Next(0, collection.Count));
+		}
+		
+		public static T SelectRandomNew<T>(this ICollection<T> collection, T current, Random? random = null)
+		{
+			random ??= Random.Shared;
+
+			T toReturn = current;
+			while (object.ReferenceEquals(toReturn, current))
+			{
+				toReturn = collection.ElementAt(random.Next(0, collection.Count));
+			}
+
+			return toReturn;
+		}
+	}
+}

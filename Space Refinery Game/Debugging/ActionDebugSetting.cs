92687c012eeb0398f60b8b765e9a15e96159a53c
Pixxl <oliverhalenius@yahoo.se>
2023-04-30 03:03:51 +0200

    add ActionDebugSetting

---
+++ Space Refinery Game/Debugging/ActionDebugSetting.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImGuiNET;
+
+namespace Space_Refinery_Game
+{
+	public sealed class ActionDebugSetting : IDebugSetting
+	{
+		public ActionDebugSetting(Action action)
+		{
+			this.action = action;
+			SettingText = null;
+		}
+
+		public Action action;
+
+		public string SettingText { get; set; }
+
+		public void DrawUIElement()
+		{
+			if (ImGui.Button(SettingText))
+			{
+				action?.Invoke();
+			}
+		}
+
+		public static implicit operator ActionDebugSetting(Action action) => new(action);
+	}
+}

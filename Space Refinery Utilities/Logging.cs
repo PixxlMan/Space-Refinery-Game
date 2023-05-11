4c837047e4b09c86a8eb1a76bc5fe80e3a0512b8
Pixxl <oliverhalenius@yahoo.se>
2023-05-11 15:09:30 +0200

    move logging to utilities and add LogError method

---
+++ Space Refinery Utilities/Logging.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Space_Refinery_Utilities;
+
+public static class Logging
+{
+	public static void Log(string logText)
+	{
+		Console.WriteLine(logText);
+	}
+
+	public static void LogError(string logText)
+	{
+		var originalColor = Console.ForegroundColor;
+		Console.ForegroundColor = ConsoleColor.Red;
+		Console.WriteLine(logText);
+		Console.ForegroundColor = originalColor;
+	}
+}

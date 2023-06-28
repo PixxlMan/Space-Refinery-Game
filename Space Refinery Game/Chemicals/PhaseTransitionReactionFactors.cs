87133b423b5ef2fb64a746a33f16bf2bb6bcb833
Pixxl <oliverhalenius@yahoo.se>
2023-06-28 21:59:45 +0200

    add todo-like comment

--- Space Refinery Game/Chemicals/PhaseTransitionReactionFactors.cs
+++ Space Refinery Game/Chemicals/PhaseTransitionReactionFactors.cs
@@ -1,5 +1,7 @@
 ﻿namespace Space_Refinery_Game;
 
+// add PhaseChangeReactionFactor? it could contain fields such as previous phase and new phase etc.
+
 public struct Boiling : ReactionFactor
 {
 	public Boiling(DecimalNumber electricalEnergy)

73eab6759b8fd70c0c5c1ac6601e72cad66e4d1b
Pixxl <oliverhalenius@yahoo.se>
2023-07-21 19:45:48 +0200

    add empty VoltageUnit and ColoumbUnit

--- Space Refinery Utilities/Units/Electrical Units.cs
+++ Space Refinery Utilities/Units/Electrical Units.cs
@@ -9,7 +9,7 @@ using Space_Refinery_Game;
 
 namespace Space_Refinery_Utilities.Units;
 
-// add more of these
+// add more of these and finish them
 
 /// <summary>
 /// 
@@ -51,6 +51,104 @@ public struct AmperageUnit : IUnit<AmperageUnit>
 	}
 
 	public bool Equals(AmperageUnit other)
+	{
+		return value.Equals(other.value);
+	}
+
+	public override int GetHashCode()
+	{
+		return value.GetHashCode();
+	}
+
+	#endregion
+}
+
+public struct VoltageUnit : IUnit<VoltageUnit>
+{
+	internal DecimalNumber value;
+
+	public VoltageUnit(DecimalNumber value)
+	{
+		this.value = value;
+	}
+
+	#region Operators and boilerplate
+
+	public static explicit operator DecimalNumber(VoltageUnit unit) => unit.value;
+
+	public static explicit operator VoltageUnit(DecimalNumber value) => new(value);
+
+	public static implicit operator VoltageUnit(int value) => new(value);
+
+	public static implicit operator VoltageUnit(double value) => new(value);
+
+	public static bool operator >(VoltageUnit a, VoltageUnit b) => a.value > b.value;
+
+	public static bool operator <(VoltageUnit a, VoltageUnit b) => a.value < b.value;
+
+	public static bool operator >=(VoltageUnit a, VoltageUnit b) => a.value >= b.value;
+
+	public static bool operator <=(VoltageUnit a, VoltageUnit b) => a.value <= b.value;
+
+	public static bool operator ==(VoltageUnit a, VoltageUnit b) => a.Equals(b);
+
+	public static bool operator !=(VoltageUnit a, VoltageUnit b) => !a.Equals(b);
+
+	public override bool Equals(object? obj)
+	{
+		return obj is VoltageUnit unit && Equals(unit);
+	}
+
+	public bool Equals(VoltageUnit other)
+	{
+		return value.Equals(other.value);
+	}
+
+	public override int GetHashCode()
+	{
+		return value.GetHashCode();
+	}
+
+	#endregion
+}
+
+public struct CoulombUnit : IUnit<CoulombUnit>
+{
+	internal DecimalNumber value;
+
+	public CoulombUnit(DecimalNumber value)
+	{
+		this.value = value;
+	}
+
+	#region Operators and boilerplate
+
+	public static explicit operator DecimalNumber(CoulombUnit unit) => unit.value;
+
+	public static explicit operator CoulombUnit(DecimalNumber value) => new(value);
+
+	public static implicit operator CoulombUnit(int value) => new(value);
+
+	public static implicit operator CoulombUnit(double value) => new(value);
+
+	public static bool operator >(CoulombUnit a, CoulombUnit b) => a.value > b.value;
+
+	public static bool operator <(CoulombUnit a, CoulombUnit b) => a.value < b.value;
+
+	public static bool operator >=(CoulombUnit a, CoulombUnit b) => a.value >= b.value;
+
+	public static bool operator <=(CoulombUnit a, CoulombUnit b) => a.value <= b.value;
+
+	public static bool operator ==(CoulombUnit a, CoulombUnit b) => a.Equals(b);
+
+	public static bool operator !=(CoulombUnit a, CoulombUnit b) => !a.Equals(b);
+
+	public override bool Equals(object? obj)
+	{
+		return obj is CoulombUnit unit && Equals(unit);
+	}
+
+	public bool Equals(CoulombUnit other)
 	{
 		return value.Equals(other.value);
 	}

31e470fe4e7821834d992dcd9a93ef07d10a113f
Pixxl <oliverhalenius@yahoo.se>
2023-09-08 14:08:33 +0200

    add Logging.LogDebug and make sure that everything is synchronized properly when printing colored messages in parallell

--- Space Refinery Utilities/Logging.cs
+++ Space Refinery Utilities/Logging.cs
@@ -1,17 +1,37 @@
-﻿namespace Space_Refinery_Utilities;
+﻿using System.Diagnostics;
 
+namespace Space_Refinery_Utilities;
+
+/// <remarks>
+/// This class is entirely thread safe.
+/// </remarks>
 public static class Logging
 {
+	private static object syncRoot = new();
+
 	public static void Log(string logText)
+	{
+		Console.WriteLine(logText);
+	}
+
+	/// <remarks>
+	/// This method is ignored in builds without the "IncludeAdditionalDebugLogging" conditional compilation symbol is defined.
+	/// </remarks>
+	[Conditional("IncludeAdditionalDebugLogging")]
+	// TODO: make this, and maybe all logging methods, faster by avoiding the slow console. Perhaps use a separate thread for that.
+	public static void LogDebug(string logText)
 	{
 		Console.WriteLine(logText);
 	}
 
 	public static void LogError(string logText)
 	{
-		var originalColor = Console.ForegroundColor;
-		Console.ForegroundColor = ConsoleColor.Red;
-		Console.WriteLine(logText);
-		Console.ForegroundColor = originalColor;
+		lock (syncRoot) // It's okay to lock here, becuase Logging an error doesn't have to be very fast.
+		{
+			var originalColor = Console.ForegroundColor;
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.WriteLine(logText);
+			Console.ForegroundColor = originalColor;
+		}
 	}
 }

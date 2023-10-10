ed7a360837a8939645df0a1679dea4f3d36aedf1
Pixxl <oliverhalenius@yahoo.se>
2023-10-10 23:19:40 +0200

    add SerializationPaths

---
+++ Space Refinery Engine/Serialization/SerializationPaths.cs
@@ -0,0 +1,12 @@
+﻿namespace Space_Refinery_Engine;
+
+public static class SerializationPaths
+{
+	public static readonly string AssetsPath = Path.Combine(Environment.CurrentDirectory, "Assets");
+
+	public static readonly string ModPath = Path.Combine(Environment.CurrentDirectory, "Mods");
+
+	public static readonly string ExtensionManifestFileExtension = ".manifest.srh.xml";
+
+	public static readonly string SerializableReferenceHandlerFileExtension = ".srh.xml";
+}

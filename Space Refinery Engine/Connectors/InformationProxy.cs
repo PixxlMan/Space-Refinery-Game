6c6c9f27b1af44744ea9e12eff68fc7b7496749e
Pixxl <oliverhalenius@yahoo.se>
2023-11-22 17:41:23 +0100

    make InformationProxy into a non entity, it never made sense as an entity anyways because it was never added to GameWorld as one

--- Space Refinery Engine/Connectors/InformationProxy.cs
+++ Space Refinery Engine/Connectors/InformationProxy.cs
@@ -1,68 +1,65 @@
 ﻿using BepuPhysics.Collidables;
 using FXRenderer;
 
-namespace Space_Refinery_Engine
+namespace Space_Refinery_Engine;
+
+// todo: make thread safe
+
+public sealed class InformationProxy
 {
-	public sealed class InformationProxy : Entity
+	public Entity ProxiedEntity { get; }
+
+	private PhysicsObject? physicsObject;
+
+	private bool enabled = false;
+
+	public InformationProxy(Connector connector)
 	{
-		public Entity ProxiedEntity { get; }
-
-		private PhysicsObject? physicsObject;
-
-		private bool enabled = false;
-
-		public InformationProxy(Connector connector)
-		{
-			ProxiedEntity = connector;
-		}
-
-		public void Enable()
-		{
-			enabled = true;
-
-			if (physicsObject is not null)
-			{
-				physicsObject.Enabled = true;
-			}
-		}
-
-		public void Disable()
-		{
-			enabled = false;
-
-			if (physicsObject is not null)
-			{
-				physicsObject.Enabled = false;
-			}
-		}
-
-		public void SetPhysicsObjectState(Transform transform, ConvexHull shape, PhysicsWorld physicsWorld)
-		{
-			physicsObject?.Destroy();
-			//if (physicsObject is null)
-			//{
-			var proxyPhysicsObject = new PhysicsObjectDescription<ConvexHull>(shape, transform, 0, true);
-
-			physicsObject = physicsWorld.AddPhysicsObject(proxyPhysicsObject, this);
-
-			physicsObject.Enabled = enabled;
-			//}
-			//else
-			//{
-			// REMEMBER TO ALSO CHANGE THE TRANSFORM HERE!!
-			//	physicsObject.World.ChangeShape(physicsObject, shape);
-			//}
-		}
-
-		public void Tick() => throw new NotSupportedException();
-
-		public void Interacted() => throw new NotSupportedException();
-
-		public void Destroy()
-		{
-			physicsObject?.Destroy();
-		}
-
-		public IInformationProvider InformationProvider => ProxiedEntity.InformationProvider;
+		ProxiedEntity = connector;
 	}
+
+	public void Enable()
+	{
+		enabled = true;
+
+		if (physicsObject is not null)
+		{
+			physicsObject.Enabled = true;
+		}
+	}
+
+	public void Disable()
+	{
+		enabled = false;
+
+		if (physicsObject is not null)
+		{
+			physicsObject.Enabled = false;
+		}
+	}
+
+	public void SetPhysicsObjectState(Transform transform, ConvexHull shape, PhysicsWorld physicsWorld)
+	{
+		physicsObject?.Destroy();
+		//if (physicsObject is null)
+		//{
+		var proxyPhysicsObject = new PhysicsObjectDescription<ConvexHull>(shape, transform, 0, true);
+
+		physicsObject = physicsWorld.AddPhysicsObject(proxyPhysicsObject, ProxiedEntity);
+
+		physicsObject.Enabled = enabled;
+		//}
+		//else
+		//{
+		// REMEMBER TO ALSO CHANGE THE TRANSFORM HERE!!
+		//	physicsObject.World.ChangeShape(physicsObject, shape);
+		//}
+	}
+
+	public void Destroy()
+	{
+		physicsObject?.Destroy();
+	}
+
+	public IInformationProvider InformationProvider => ProxiedEntity.InformationProvider;
 }

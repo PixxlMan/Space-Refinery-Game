0e6adfaf11875c24b6e7cc6adbcac1253de6c30b
Pixxl <oliverhalenius@yahoo.se>
2024-02-26 18:39:52 +0100

    add Tests project and add InternalEnergy tests

---
+++ Tests/InternalEnergy.cs
@@ -0,0 +1,58 @@
+﻿using Space_Refinery_Engine;
+using Space_Refinery_Engine.Audio;
+using Space_Refinery_Utilities;
+using Space_Refinery_Utilities.Units;
+using System.Diagnostics;
+
+namespace Tests
+{
+	[TestClass]
+	public class InternalEnergy
+	{
+		[TestInitialize]
+		public void Initialize()
+		{
+			ReferenceHandler = new();
+
+			ReferenceHandler.EnterAllowEventualReferenceMode(false);
+			{
+				ResourceDeserialization.DeserializeIntoGlobalReferenceHandler(ReferenceHandler, new(), includeGameExtension: false);
+			}
+			ReferenceHandler.ExitAllowEventualReferenceMode();
+
+			WaterChemical = ChemicalType.Water;
+
+			WaterResource = WaterChemical.GetResourceTypeForPhase(ChemicalPhase.Liquid);
+
+			WaterTemperatureEnergy = WaterChemical.SpecificHeatCapacity * WaterMass * WaterTemperature;
+		}
+
+		public SerializationReferenceHandler ReferenceHandler;
+			   
+		public ChemicalType WaterChemical;
+			   
+		public ResourceType WaterResource;
+			   
+		public MassUnit WaterMass = 1; // 1 kilogram of water
+			   
+		public TemperatureUnit WaterTemperature = 20 + 273.15; // 20℃
+			   
+		public EnergyUnit WaterTemperatureEnergy;
+
+		[TestMethod]
+		public void TemperatureToEnergy()
+		{
+			EnergyUnit internalTemperatureEnergy = ChemicalType.TemperatureToInternalEnergy(WaterResource, WaterTemperature, WaterMass);
+
+			Assert.AreEqual<DecimalNumber>((DecimalNumber)internalTemperatureEnergy, (DecimalNumber)WaterTemperatureEnergy);
+		}
+
+		[TestMethod]
+		public void EnergyToTemperature()
+		{
+			TemperatureUnit temperature = ChemicalType.InternalEnergyToTemperature(WaterResource, WaterTemperatureEnergy, WaterMass);
+
+			Assert.AreEqual<DecimalNumber>((DecimalNumber)temperature, (DecimalNumber)WaterTemperature);
+		}
+	}
+}

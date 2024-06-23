70443ecb7bb04689b64e694d44e8daa67ab046a3
Pixxl <oliverhalenius@yahoo.se>
2024-06-23 02:25:04 +0200

    rename LoadCached to LoadVertexFragmentCached and add LoadComputeCached and add defensive asserts

--- Space Refinery Game Renderer/ShaderLoader.cs
+++ Space Refinery Game Renderer/ShaderLoader.cs
@@ -1,4 +1,7 @@
-﻿using Veldrid;
+﻿using Space_Refinery_Utilities;
+using System.Diagnostics;
+using Veldrid;
+using Veldrid.SPIRV;
 
 namespace Space_Refinery_Game_Renderer;
 
@@ -13,17 +16,46 @@ public sealed class ShaderLoader
 		this.graphicsWorld = graphicsWorld;
 	}
 
-	public Shader[] LoadCached(string shaderName)
+	public Shader[] LoadVertexFragmentCached(string shaderName)
 	{
-		if (shaderCache.ContainsKey(shaderName))
+		if (!shaderCache.TryGetValue(shaderName, out Shader[]? shaders))
 		{
-			return shaderCache[shaderName];
+			Logging.LogScopeStart($"Vertex-fragment shader loading '{shaderName}'");
+
+			var path = Path.Combine(Environment.CurrentDirectory, "Shaders");
+			shaders = Utils.LoadShaders(path, shaderName, graphicsWorld.Factory);
+
+			shaderCache.Add(shaderName, shaders);
+
+			Logging.Log($"Loaded and cached shader '{shaderName}' ('{shaders[0].Name}', '{shaders[1].Name}') with stages '{shaders[0].Stage}', '{shaders[1].Stage}' in {path}");
+			Logging.LogScopeEnd();
 		}
 
-		var shader = Utils.LoadShaders(Path.Combine(Environment.CurrentDirectory, "Shaders"), shaderName, graphicsWorld.Factory);
+		Debug.Assert(shaders[0].Stage == ShaderStages.Vertex && shaders[1].Stage == ShaderStages.Fragment);
+		return shaders;
+	}
 
-		shaderCache.Add(shaderName, shader);
+	public Shader LoadComputeCached(string shaderName)
+	{
+		Shader shader;
+		if (shaderCache.TryGetValue(shaderName, out Shader[]? shaders))
+		{
+			shader = shaders[0];
+		}
+		else
+		{
+			Logging.LogScopeStart($"Compute shader loading '{shaderName}'");
 
+			var path = Path.Combine(Environment.CurrentDirectory, "Shaders");
+			shader = Utils.LoadShader(path, shaderName, graphicsWorld.Factory);
+
+			shaderCache.Add(shaderName, [shader]);
+
+			Logging.Log($"Loaded and cached shader '{shaderName}' ('{shader.Name}') with stage '{shader.Stage}' in {path}");
+			Logging.LogScopeEnd();
+		}
+
+		Debug.Assert(shader.Stage == ShaderStages.Compute);
 		return shader;
 	}
 }

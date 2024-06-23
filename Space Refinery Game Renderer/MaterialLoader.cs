e375622f686e5b9a771c9dd46b5aa3e3b8275dcd
Pixxl <oliverhalenius@yahoo.se>
2024-06-24 01:19:57 +0200

    add LoadGLTFMaterial

--- Space Refinery Game Renderer/MaterialLoader.cs
+++ Space Refinery Game Renderer/MaterialLoader.cs
@@ -1,19 +1,21 @@
-﻿namespace Space_Refinery_Game_Renderer;
+﻿using SharpGLTF.Materials;
+
+namespace Space_Refinery_Game_Renderer;
 
 public sealed class MaterialLoader(GraphicsWorld graphicsWorld)
 {
-	private Dictionary<MaterialLoadingDescription, Material> materialCache = new();
+	private Dictionary<string, Material> materialCache = new();
 
 	private GraphicsWorld graphicsWorld = graphicsWorld;
 
 	public Material LoadCached(MaterialLoadingDescription materialTexturePaths)
 	{
-		if (materialCache.TryGetValue(materialTexturePaths, out Material? value))
+		if (materialCache.TryGetValue(materialTexturePaths.Name, out Material? material))
 		{
-			return value;
+			return material;
 		}
 
-		var material = Material.LoadMaterial(
+		material = Material.LoadMaterial(
 			graphicsWorld.GraphicsDevice,
 			graphicsWorld.Factory,
 			materialTexturePaths.Name,
@@ -23,12 +25,38 @@ public sealed class MaterialLoader(GraphicsWorld graphicsWorld)
 			materialTexturePaths.AmbientOcclusionTexturePath
 			);
 
-		materialCache.Add(materialTexturePaths, material);
+		materialCache.Add(materialTexturePaths.Name, material);
+
+		return material;
+	}
+
+	public Material LoadGLTFMaterial(MaterialBuilder gltfMaterial)
+	{
+		if (materialCache.TryGetValue(gltfMaterial.Name, out Material? material))
+		{
+			return material;
+		}
+
+		var diffuseImage = gltfMaterial.GetChannel(KnownChannel.BaseColor).GetValidTexture().PrimaryImage.Content;
+		var metallicImage = gltfMaterial.GetChannel(KnownChannel.Normal).GetValidTexture().PrimaryImage.Content;
+		var roughnessImage = gltfMaterial.GetChannel(KnownChannel.MetallicRoughness).GetValidTexture().PrimaryImage.Content;
+		//var ambientOcclusionImage = gltfMaterial.GetChannel(KnownChannel.Normal).GetValidTexture().PrimaryImage.Content;
+
+		material = Material.FromTextures(
+			graphicsWorld.GraphicsDevice,
+			graphicsWorld.Factory,
+			gltfMaterial.Name,
+			Material.CreateTextureFromBytes(graphicsWorld.GraphicsDevice, graphicsWorld.Factory, diffuseImage.Content.Span),
+			Material.CreateTextureFromBytes(graphicsWorld.GraphicsDevice, graphicsWorld.Factory, metallicImage.Content.Span),
+			Material.CreateTextureFromBytes(graphicsWorld.GraphicsDevice, graphicsWorld.Factory, roughnessImage.Content.Span),
+			RenderingResources.DefaultTexture//Material.CreateTextureFromBytes(graphicsWorld.GraphicsDevice, graphicsWorld.Factory, ambientOcclusionImage.Content.Span)
+			);
+
+		materialCache.Add(gltfMaterial.Name, material);
 
 		return material;
 	}
 }
-
 
 public record struct MaterialLoadingDescription
 	(

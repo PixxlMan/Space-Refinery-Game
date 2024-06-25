007c6bca793869d6a1c371c3d1ef66fa30636c53
Pixxl <oliverhalenius@yahoo.se>
2024-06-25 04:09:01 +0200

    add tone mapping to FullscreenQuad

--- Space Refinery Game Renderer/Shaders/FullscreenQuad-fragment.glsl
+++ Space Refinery Game Renderer/Shaders/FullscreenQuad-fragment.glsl
@@ -6,9 +6,39 @@ layout(set = 0, binding = 1) uniform sampler SourceSampler;
 layout(location = 0) in vec2 fsin_TexCoords;
 layout(location = 0) out vec4 OutputColor;
 
+// CREDIT: MIT, https://github.com/dmnsgn/glsl-tone-map/blob/main/aces.glsl
+
+// Narkowicz 2015, "ACES Filmic Tone Mapping Curve"
+vec3 aces(vec3 x)
+{
+  const float a = 2.51;
+  const float b = 0.03;
+  const float c = 2.43;
+  const float d = 0.59;
+  const float e = 0.14;
+  return clamp((x * (a * x + b)) / (x * (c * x + d) + e), 0.0, 1.0);
+}
+
+vec3 lottes(vec3 x) {
+  const vec3 a = vec3(1.6);
+  const vec3 d = vec3(0.977);
+  const vec3 hdrMax = vec3(8.0);
+  const vec3 midIn = vec3(0.18);
+  const vec3 midOut = vec3(0.267);
+
+  const vec3 b =
+      (-pow(midIn, a) + pow(hdrMax, a) * midOut) /
+      ((pow(hdrMax, a * d) - pow(midIn, a * d)) * midOut);
+  const vec3 c =
+      (pow(hdrMax, a * d) * pow(midIn, a) - pow(hdrMax, a) * pow(midIn, a * d) * midOut) /
+      ((pow(hdrMax, a * d) - pow(midIn, a * d)) * midOut);
+
+  return pow(x, a) / (pow(x, a * d) * b + c);
+}
+
 void main()
 {
     vec4 color = texture(sampler2D(SourceTexture, SourceSampler), fsin_TexCoords);
 
-    OutputColor = color;
+    OutputColor = vec4(lottes(color.xyz), 1);
 }

9c4eb8832ba22e0466bde6d9d22cc5c6e803ab89
Pixxl <oliverhalenius@yahoo.se>
2024-08-07 21:26:04 +0200

    add new VertexData type

---
+++ Space Refinery Engine/Graphics/VertexData.cs
@@ -0,0 +1,25 @@
+﻿using System.Numerics;
+
+namespace Space_Refinery_Engine.Renderer;
+
+public struct VertexData
+{
+	public const byte SizeInBytes = 48;
+	public const byte NormalOffset = 12;
+	public const byte TextureCoordinatesOffset = 24;
+	public const byte TangentOffset = 32;
+	public const byte ElementCount = 4;
+
+	public readonly Vector3 Position;
+	public readonly Vector3 Normal;
+	public readonly Vector2 TextureCoordinates;
+	public readonly Vector3 Tangent;
+
+	public VertexData(Vector3 position, Vector3 normal, Vector2 texCoords, Vector3 tangent)
+	{
+		Position = position;
+		Normal = normal;
+		TextureCoordinates = texCoords;
+		Tangent = tangent;
+	}
+}
